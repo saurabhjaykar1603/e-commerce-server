59ab4de3e0cfff308492adc062afaba6fb6d32c9
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-10-20 21:55:02 +0530

    feat : database connect successfully

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
 const app = express();
 app.use(express.json());
 
 app.get("/health", (req, res) => {
   res.send("all good ");
 });
+
+// database connection
+const connectMongoDB = async (req, res) => {
+  const conn = await mongoose.connect(process.env.MONGODB_URI);
+  if (conn) {
+    console.log("mongoDB connection successful");
+  }
+};
+connectMongoDB();
 
 const PORT = 5000;
 
