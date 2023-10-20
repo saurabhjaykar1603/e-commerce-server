3d5cf0df4460e4e60d252b3bbbf14f2e3d25722a
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-10-20 22:01:57 +0530

    feat : schema design for model

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import Product from "./src/models/product";
 dotenv.config();
 
 const app = express();
